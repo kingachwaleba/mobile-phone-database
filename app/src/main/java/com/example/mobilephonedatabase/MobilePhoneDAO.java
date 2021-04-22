fafcfbba0520303e88234c70217751da9f6e02fa
kingachwaleba <kingach6@gmail.com>
2021-04-22 11:55:08 +0200

    Created Data Access Object interface

--- app/src/main/java/com/example/mobilephonedatabase/MobilePhoneDAO.java
+++ app/src/main/java/com/example/mobilephonedatabase/MobilePhoneDAO.java
@@ -1,4 +1,29 @@
 package com.example.mobilephonedatabase;
 
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
 public interface MobilePhoneDAO {
+
+    // The operation will be aborted when there is any conflict
+    @Insert(onConflict = OnConflictStrategy.ABORT)
+    void insert(MobilePhone mobilePhone);
+
+    @Delete
+    void delete(MobilePhone mobilePhone);
+
+    @Query("DELETE FROM mobilephone")
+    void deleteAll();
+
+    // Return LiveData container with the list of the mobile phones
+    // It enables to received notifications when data is changed
+    @Query("SELECT * FROM mobilephone ORDER BY model")
+    LiveData<List<MobilePhone>> getAlphabetizedMobilePhones();
 }
