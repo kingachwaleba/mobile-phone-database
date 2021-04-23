b33a19db934a329a10962315792f3e4e70551b59
kingachwaleba <kingach6@gmail.com>
2021-04-23 11:17:37 +0200

    Created a class represents database

--- app/src/main/java/com/example/mobilephonedatabase/MobilePhoneRoomDatabase.java
+++ app/src/main/java/com/example/mobilephonedatabase/MobilePhoneRoomDatabase.java
@@ -1,4 +1,51 @@
 package com.example.mobilephonedatabase;
 
-public class MobilePhoneRoomDatabase {
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Database(entities = {MobilePhone.class}, version = 1, exportSchema = false)
+public abstract class MobilePhoneRoomDatabase extends RoomDatabase {
+
+    // Abstract method returns DAO
+    public abstract MobilePhoneDAO mobilePhoneDAO();
+
+    // Singleton implementation
+    private static volatile MobilePhoneRoomDatabase INSTANCE;
+
+    static MobilePhoneRoomDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (MobilePhoneRoomDatabase.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            MobilePhoneRoomDatabase.class, "mobile_phones")
+                            .addCallback(sRoomDatabaseCallback)
+                            .fallbackToDestructiveMigration()
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    private static final int NUMBER_OF_THREADS = 4;
+    static final ExecutorService databaseWriteExecutor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+
+    private static RoomDatabase.Callback sRoomDatabaseCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+
+            databaseWriteExecutor.execute(() -> {
+                MobilePhoneDAO mobilePhoneDAO = INSTANCE.mobilePhoneDAO();
+            });
+        }
+    };
 }
