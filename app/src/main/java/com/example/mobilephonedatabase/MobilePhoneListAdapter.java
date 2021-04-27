c974c86b4100d822b02542ae872e1d01704eedb4
kingachwaleba <kingach6@gmail.com>
2021-04-27 16:46:48 +0200

    Created an adapter for the RecyclerView element

--- app/src/main/java/com/example/mobilephonedatabase/MobilePhoneListAdapter.java
+++ app/src/main/java/com/example/mobilephonedatabase/MobilePhoneListAdapter.java
@@ -1,4 +1,50 @@
 package com.example.mobilephonedatabase;
 
-public class MobilePhoneListAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MobilePhoneListAdapter extends RecyclerView.Adapter<MobilePhoneListAdapter.MobilePhoneViewHolder> {
+
+    private List<MobilePhone> mobilePhoneList;
+    private LayoutInflater layoutInflater;
+
+    public MobilePhoneListAdapter(Context context) {
+        layoutInflater = LayoutInflater.from(context);
+        this.mobilePhoneList = null;
+    }
+
+    @Override
+    public int getItemCount() {
+        return mobilePhoneList != null ? mobilePhoneList.size() : 0;
+    }
+
+    public void setMobilePhoneList(List<MobilePhone> mobilePhoneList) {
+        this.mobilePhoneList = mobilePhoneList;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public MobilePhoneViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MobilePhoneViewHolder holder, int position) {
+
+    }
+
+    public class MobilePhoneViewHolder extends RecyclerView.ViewHolder {
+
+        public MobilePhoneViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
