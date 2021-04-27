8d6fa72e56e6a7d47d67d1be7fbd6e76eede2b99
kingachwaleba <kingach6@gmail.com>
2021-04-27 19:18:05 +0200

    Set an adapter as a recycler view

--- app/src/main/java/com/example/mobilephonedatabase/MainActivity.java
+++ app/src/main/java/com/example/mobilephonedatabase/MainActivity.java
@@ -1,14 +1,26 @@
 package com.example.mobilephonedatabase;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private MobilePhoneListAdapter mobilePhoneListAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Set an adapter in the list
+        // Set the layout of the elements of the list
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        mobilePhoneListAdapter = new MobilePhoneListAdapter(this);
+        recyclerView.setAdapter(mobilePhoneListAdapter);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
