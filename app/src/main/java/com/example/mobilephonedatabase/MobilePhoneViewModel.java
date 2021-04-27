11483de3bd61a9ce4580ec46e37c58e3fb3b19e5
kingachwaleba <kingach6@gmail.com>
2021-04-27 15:37:45 +0200

    Created view model

--- app/src/main/java/com/example/mobilephonedatabase/MobilePhoneViewModel.java
+++ app/src/main/java/com/example/mobilephonedatabase/MobilePhoneViewModel.java
@@ -1,4 +1,33 @@
 package com.example.mobilephonedatabase;
 
-public class MobilePhoneViewModel {
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class MobilePhoneViewModel extends AndroidViewModel {
+
+    private final MobilePhoneRepository mobilePhoneRepository;
+    private final LiveData<List<MobilePhone>> allMobilePhones;
+
+    public MobilePhoneViewModel(@NonNull Application application) {
+        super(application);
+
+        mobilePhoneRepository = new MobilePhoneRepository(application);
+        allMobilePhones = mobilePhoneRepository.getAllMobilePhones();
+    }
+
+    // Return all mobile phones
+    public LiveData<List<MobilePhone>> getAllMobilePhones() {
+        return allMobilePhones;
+    }
+
+    // Delete all mobile phones
+    public void deleteAll() {
+        mobilePhoneRepository.deleteAll();
+    }
+
 }
