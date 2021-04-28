d9f20ac3769f578cd1749f3f74f47ee8aebda75e
kingachwaleba <kingach6@gmail.com>
2021-04-29 00:11:13 +0200

    Overrided onCreate method

--- app/src/main/java/com/example/mobilephonedatabase/InsertActivity.java
+++ app/src/main/java/com/example/mobilephonedatabase/InsertActivity.java
@@ -1,4 +1,31 @@
 package com.example.mobilephonedatabase;
 
-public class InsertActivity {
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class InsertActivity extends AppCompatActivity {
+
+    EditText producerName;
+    EditText modelName;
+    EditText androidVersion;
+    EditText webPage;
+
+    Button webSiteButton;
+    Button cancelButton;
+    Button saveButton;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.insert_layout);
+
+        producerName = findViewById(R.id.producerNameInput);
+        modelName = findViewById(R.id.modelNameInput);
+        androidVersion = findViewById(R.id.androidVersionInput);
+        webPage = findViewById(R.id.webSiteInput);
+    }
 }
